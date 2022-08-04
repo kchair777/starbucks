3837d4823f7f54433b4ed4687c87919aa682828c
kchair777 <kchstar@gmail.com>
2022-08-04 10:52:40 +0900

    공통 모듈 분리

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,26 +1,9 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function(){
-  // logic 입력
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
 
 const badgeEl = document.querySelector('header  .badges');
 const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
-  console.log('window.scrollY');
+  console.log(window.scrollY);
   if (window.scrollY > 500) { 
    
     gsap.to(badgeEl, .6, {
@@ -148,5 +131,3 @@ spyEls.forEach(function(spyEl){
   .addTo(new ScrollMagic.Controller());
 });
 
-const thiYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
